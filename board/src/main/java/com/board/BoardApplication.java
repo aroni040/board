f5351fc4b6ad575c2a98676ed46d02bb459af694
aroni040 <hongeunhappy@gmail.com>
2020-12-08 09:47:14 +0900

    pring-boot-starter-test

--- board/src/main/java/com/board/BoardApplication.java
+++ board/src/main/java/com/board/BoardApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardApplication {
-
+	
 	public static void main(String[] args) {
 		//System.setProperty("spring.profiles.default", "local");
-		SpringApplication.run(BoardApplication.class, args);
-		
-	}
-
+		SpringApplication.run(BoardApplication.class, args);						
+	}		
 }
